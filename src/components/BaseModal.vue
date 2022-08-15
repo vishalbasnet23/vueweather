42cad34a5eca30e720572c3ce9f63cd034cc10a2
Vishalbasnet23 <vishalbasnet23@gmail.com>
2022-08-15 13:56:36 +0545

    1. Toggle modal method added 2. Emitted close modal event from modal

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    v-show="modalActive"
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+  >
+    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-sm">
+      <slot />
+      <button
+        class="text-white mt-8 bg-primary py-2 px-6"
+        @click="$emit('close-modal')"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    console.log(props);
+  },
+};
+</script>
