2b8a0e966d0f8d88cbf77f0d34b3f8131237d583
Vishalbasnet23 <vishalbasnet23@gmail.com>
2022-08-15 13:25:50 +0545

    1. Router link added to home 2. Other icons added

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -3,15 +3,26 @@
     <nav
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
-      <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-sun text-2xl"></i>
-        <p class="text-2xl">Local Weather</p>
+      <Routerlink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">Local Weather</p>
+        </div>
+      </Routerlink>
+      <div class="flex gap-3 flex-1 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-secondary duration-150 cursor-pointer"
+        />
+        <i
+          class="fa-solid fa-plus text-xl hover:text-secondary duration-150 cursor-pointer"
+        />
       </div>
     </nav>
   </header>
 </template>
 
 <script>
+import { RouterLink } from "vue-router";
 export default {
   setup() {
     return {};
