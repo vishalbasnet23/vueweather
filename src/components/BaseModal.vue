706445dd64bb5ef941219678b6a94d2c8844cac2
Vishalbasnet23 <vishalbasnet23@gmail.com>
2022-08-15 15:04:29 +0545

    1. Positionstack api added to get places

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,22 +1,27 @@
 <template>
-  <Transition name="modal-outer">
-    <div
-      v-show="modalActive"
-      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-    >
-      <Transition name="modal-inner">
-        <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-sm">
-          <slot />
-          <button
-            class="text-white mt-8 bg-primary py-2 px-6"
-            @click="$emit('close-modal')"
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-sm"
           >
-            Close
-          </button>
-        </div>
-      </Transition>
-    </div>
-  </Transition>
+            <slot />
+            <button
+              class="text-white mt-8 bg-primary py-2 px-6"
+              @click="$emit('close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
-
 <template>
-  <main>Hello</main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        placeholder="Search"
+        @input="getSearchResults"
+        v-model="searchQuery"
+        class="py=2 px-1 w-full bg-transparent border-b focus:border-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="searchResults"
+      >
+        <li
+          v-for="(searchResultValue, searchResultKey) in searchResults"
+          :key="searchResultKey"
+        >
+          {{ searchResultValue }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+<script>
+import { ref } from "vue";
+import axios from "axios";
+export default {
+  setup() {
+    const searchQuery = ref("");
+    const searchResults = ref(null);
+    const queryTimeOut = ref(null);
+    function getSearchResults() {
+      clearTimeout(queryTimeOut.value);
+      queryTimeOut.value = setTimeout(async () => {
+        if (searchQuery.value !== "") {
+          const result = await axios.get(
+            `http://api.positionstack.com/v1/forward?access_key=71c87c2d5233412bc09bd8e2b61c8e8e&query=${searchQuery.value}&fields=results.label`
+          );
+          const allLabels = result.data.data.map((obj) => obj.label);
+          searchResults.value = [...new Set(allLabels)];
+          return;
+        }
+        searchResults.value = null;
+      }, 300);
+    }
+    return { getSearchResults, searchQuery, searchResults };
+  },
+};
+</script>
